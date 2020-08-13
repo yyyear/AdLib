4c840a33b9d6a8bdd231b0a32712e300396250d9
aeagle <aeagle@qq.com>
2020-08-13 14:45:08 +0800

    123

---
+++ ios/AdLib.framework/Versions/A/Headers/AdLib.h
@@ -0,0 +1,13 @@
+//Created by 张亚举 on 2019/3/12. Copyright © 2019 YYY. All rights reserved.
+// <#文件说明#>
+	
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AdLib : NSObject
++ (void) testLib;
+@end
+
+NS_ASSUME_NONNULL_END
